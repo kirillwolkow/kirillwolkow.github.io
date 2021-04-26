541a20fd4bec392063acb577bf175585bd868e58
kirillwolkow <bhustatime@gmail.com>
2021-04-26 10:21:35 +0200

    Added Social Media Link to the Contact page and removed Twitter from Social Share Buttons

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 /*
-Twitter
-https://twitter.com/share?url=[post-url]&text=[post-title]&via=[via]&hashtags=[hashtags]
 
 Github
 https://github.com/share?url=[post-url]&text=[post-title]
@@ -10,7 +8,6 @@ https://reddit.com/submit?url=[post-url]&title=[post-title]
 
 */
 
-const twitterBtn = document.querySelector(".twitter-btn");
 const githubBtn = document.querySelector(".github-btn");
 const redditBtn = document.querySelector(".reddit-btn");
 
@@ -18,7 +15,6 @@ function init() {
     let postUrl = encodeURI(document.location.href);
     let postTitle = encodeURI("Hi everyone, please check this out: ");
 
-    twitterBtn.setAttribute("href", 'https://twitter.com/share?url=${postUrL}&text=${postTitle}');
     githubBtn.setAttribute("href", 'https://github.com/share?url=${postUrl}&text=${postTitle}');
     redditBtn.setAttribute("href", 'https://reddit.com/submit?url=${postUrl}&title=${postTitle}');
 }
