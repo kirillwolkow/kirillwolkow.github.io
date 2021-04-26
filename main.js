21a110d1fd35180594b9e535fc2393031684a6d1
kirillwolkow <bhustatime@gmail.com>
2021-04-26 10:05:28 +0200

    Made some corrections and added a journal entry

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ Twitter
 https://twitter.com/share?url=[post-url]&text=[post-title]&via=[via]&hashtags=[hashtags]
 
 Github
-
+https://github.com/share?url=[post-url]&text=[post-title]
 
 Reddit
 https://reddit.com/submit?url=[post-url]&title=[post-title]
@@ -18,9 +18,9 @@ function init() {
     let postUrl = encodeURI(document.location.href);
     let postTitle = encodeURI("Hi everyone, please check this out: ");
 
-    twitterBtn.setAttribute("href", 'https://twitter.com/share?url=${postURL}&text=${postTitle}');
-    githubBtn.setAttribute("href", 'https://github.com/share?url=${postURL}&text=${postTitle}');
-    redditBtn.setAttribute("href", 'https://reddit.com/submit?url=${postUrl}&title=${}postTitle');
+    twitterBtn.setAttribute("href", 'https://twitter.com/share?url=${postUrL}&text=${postTitle}');
+    githubBtn.setAttribute("href", 'https://github.com/share?url=${postUrl}&text=${postTitle}');
+    redditBtn.setAttribute("href", 'https://reddit.com/submit?url=${postUrl}&title=${postTitle}');
 }
 
 init();
